f4ec0ef01bb2ca6db41a13bdd21406d91bc7cdd0
TimurBrazhko <timurbrazhko@gmail.com>
2024-09-09 17:52:43 +0600

    2 homework

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
+    rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from posts.models import Post
+
 
 def test_view(request):
     return HttpResponse("Wassup")
@@ -8,3 +10,13 @@ def test_view(request):
 
 def main_page_view(request):
     return render(request, 'base.html')
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, 'post/post_list.html', context={'posts': posts})
+
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'post/post.detail.html', context={'post': post})
