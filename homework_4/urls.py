fc19a6db8dcf5767683e47b97542760b5861f7e2
TimurBrazhko <timurbrazhko@gmail.com>
2024-09-12 10:10:16 +0600

    3 hw done

--- homework_4/urls.py
+++ homework_4/urls.py
@@ -6,17 +6,19 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views. home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
 """
 from django.contrib import admin
 from django.urls import path
 from posts.views import test_view, main_page_view, post_list_view, post_detail_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,4 @@ urlpatterns = [
     path('', main_page_view),
     path('posts/', post_list_view),
     path('posts/<int:post_id>/', post_detail_view),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Post(models.Model):
+    image = models.ImageField(blank=True, null=True, upload_to='posts')
     title = models.CharField(max_length=200)
     content = models.TextField(null=True, blank=True)
     rate = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True, blank=True, related_name='posts'
+    )
+    tag = models.ManyToManyField(Tag, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
